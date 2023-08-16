3f6cbe2648b9aa5a6f52665dcf6da3b637fa3d64
Almaz Balbaev <kujitengu@gmail.com>
2023-08-16 20:19:09 +0600

    imported UserController & UserService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { DatabaseModule } from './database.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
@@ -10,7 +12,7 @@ import { User } from './user/user.entity';
     DatabaseModule,
     TypeOrmModule.forFeature([User]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
