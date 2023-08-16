320a991e0bbd04809e134569a40b28b2a3a2a2c8
Almaz Balbaev <kujitengu@gmail.com>
2023-08-16 20:21:12 +0600

    added globalPipes logic to return validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        const validationErrors: ValidationErrors = {};
+
+        errors.forEach((error) => {
+          const constraints = error.constraints;
+
+          if (constraints) {
+            validationErrors[error.property] = Object.keys(constraints).map(
+              (key) => constraints[key],
+            );
+          }
+        });
+
+        return new BadRequestException(validationErrors);
+      },
+    }),
+  );
+
+  await app.listen(8000);
 }
 bootstrap();
