bfe642f2bf1c8d4f6e1f63eae4178148af9beb1f
Almaz Balbaev <kujitengu@gmail.com>
2023-08-17 20:51:49 +0600

    login and logout logic added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Patch,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { TokenAuthGuard } from '../auth/token-auth.guard';
+import { CurrentUser } from '../auth/currentUser.decorator';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UserController {
@@ -16,5 +28,31 @@ export class UserController {
       body.password,
       body.age,
     );
+  }
+
+  @Patch('edit')
+  @UseGuards(TokenAuthGuard)
+  async editUser(@CurrentUser() user: User, @Body() body: Partial<User>) {
+    return this.userService.editUser(
+      user.id,
+      body.email,
+      body.firstName,
+      body.lastName,
+      body.patronymic,
+      body.password,
+      body.age,
+    );
+  }
+
+  @Post('login')
+  @UseInterceptors(ClassSerializerInterceptor)
+  async loginUser(@Body() body: { email: string; password: string }) {
+    return this.userService.loginUser(body.email, body.password);
+  }
+
+  @Delete('logout')
+  @UseGuards(TokenAuthGuard)
+  async logoutUser(@CurrentUser() user: User) {
+    return this.userService.logoutUser(user.id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -18,7 +22,11 @@ export class UserService {
     password: string,
     age: number,
   ): Promise<User> {
-    await this.getUserByEmail(email);
+    const existUser = await this.userRepository.findOne({ where: { email } });
+
+    if (existUser) {
+      throw new BadRequestException('This email is already registered!');
+    }
 
     const user = await this.userRepository.create({
       email,
@@ -33,13 +41,51 @@ export class UserService {
     return await this.userRepository.save(user);
   }
 
+  async loginUser(email: string, password: string): Promise<User> {
+    const existUser = await this.userRepository.findOne({ where: { email } });
+
+    if (!existUser) {
+      throw new NotFoundException('This email not found!');
+    }
+
+    const isMatch = await existUser.checkPassword(password);
+
+    if (!isMatch) {
+      throw new NotFoundException('Password is wrong');
+    }
+
+    await existUser.generateToken();
+    return await this.userRepository.save(existUser);
+  }
+
+  async editUser(
+    id: number,
+    email: string,
+    firstName: string,
+    lastName: string,
+    patronymic: string,
+    password: string,
+    age: number,
+  ) {
+    const existUser = await this.userRepository.findOne({ where: { id } });
+
+    if (!existUser) {
+      throw new BadRequestException('User not found!');
+    }
+  }
+
+  async logoutUser(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+    await user.generateToken();
+    await this.userRepository.save(user);
+    return { message: 'Logout success' };
+  }
+
   private async getUserByEmail(email: string) {
     const user = await this.userRepository.findOne({ where: { email } });
 
-    if (user) {
-      throw new BadRequestException({
-        email: ['This email is already registered!'],
-      });
+    if (!user) {
+      throw new BadRequestException('This email not found!');
     }
 
     return user;
