d437a89b6ab02f9d5014d225a23582a0465b52f8
Almaz Balbaev <kujitengu@gmail.com>
2023-08-18 18:43:48 +0600

    added swagger decorators

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -3,29 +3,37 @@ import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 import { Exclude } from 'class-transformer';
 import { SALT_WORK_FACTOR } from '../constants';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({ unique: true })
   email: string;
 
   @Exclude()
+  @ApiHideProperty()
   @Column()
   password: string;
 
   @Column()
+  @ApiProperty()
   firstName: string;
 
   @Column()
+  @ApiProperty()
   lastName: string;
 
   @Column({ nullable: true })
+  @ApiProperty({ required: false })
   patronymic: string;
 
   @Column()
+  @ApiProperty()
   age: number;
 
   @Column()
