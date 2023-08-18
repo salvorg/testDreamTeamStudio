8cf858f5178a42dc34ce47bce6b45a01e3f807c4
Almaz Balbaev <kujitengu@gmail.com>
2023-08-18 18:43:42 +0600

    added swagger decorators

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -8,12 +8,15 @@ import {
   Matches,
   Length,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail({}, { message: 'Некорректный формат email' })
   @IsNotEmpty({ message: 'Поле email не должно быть пустым' })
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Поле пароля не должно быть пустым' })
   @IsString({ message: 'Поле пароля должно быть строкой' })
   @Length(8, 20, { message: 'Пароль должен содержать от 8 до 20 символов' })
@@ -26,21 +29,25 @@ export class CreateUserDto {
   )
   password: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Поле имени не должно быть пустым' })
   @IsString({ message: 'Поле имени должно быть строкой' })
   @Matches(/^[a-zA-Zа-яА-Я-]+$/, { message: 'Некорректный формат имени' })
   firstName: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Поле фамилии не должно быть пустым' })
   @IsString({ message: 'Поле фамилии должно быть строкой' })
   @Matches(/^[a-zA-Zа-яА-Я-]+$/, { message: 'Некорректный формат фамилии' })
   lastName: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString({ message: 'Поле отчества должно быть строкой' })
   @Matches(/^[a-zA-Zа-яА-Я-]+$/, { message: 'Некорректный формат отчества' })
   patronymic: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Поле age не должно быть пустым' })
   @IsInt({ message: 'Поле age должно быть числом' })
   @Min(0)
